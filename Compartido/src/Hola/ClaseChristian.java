b819b4aebb868f96d760c9a7a994e5beb569a566
Christian Ruiz <crnoain@gmail.com>
2018-04-11 17:30:12 +0200

    Nueva clase

---
+++ Compartido/src/Hola/ClaseChristian.java
@@ -0,0 +1,13 @@
+package Hola;
+
+public class ClaseChristian {
+
+public static void main(String[] args) {
+		
+		System.out.println("Hola Git.");
+		System.out.println("Hola Tania.");
+		System.out.println("Hola soy Tania y voy a modificar esta clase");
+		System.out.println("Hola es mi segunda modificacion");
+		System.out.println("Hola soy Christian");
+	}
+}
