4b2930c4992299ff718bb05af6fb711bc8853032
Moretti <blog@standardinet.com>
2018-04-11 08:04:05 -0700

    Modificado por mi, oscar.

--- Compartido/src/Hola/holamundo.java
+++ Compartido/src/Hola/holamundo.java
@@ -4,6 +4,7 @@ public class holamundo {
 	
 	public static void main (String ars[]){
 		System.out.println("Hola, Buenas Tardes");
+		System.out.println("Hola, este soy yo, Oscar. Lo he modificado.");
 	}
 
 }
