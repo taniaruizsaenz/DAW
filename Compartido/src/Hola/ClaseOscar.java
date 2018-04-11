323a3b2835c3bbb9a262fa8bf64f3e1e56f076ea
Moretti <blog@standardinet.com>
2018-04-11 08:48:03 -0700

    Ultimo cambio

--- Compartido/src/Hola/ClaseOscar.java
+++ Compartido/src/Hola/ClaseOscar.java
@@ -6,13 +6,11 @@ public class ClaseOscar {
 		
 		System.out.println("Hola Git.");
 		System.out.println("Hola Tania.");
-<<<<<<< HEAD
-		System.out.println("Esta es otra modificación mía, oscar.");
 
-=======
+		System.out.println("Esta es otra modificación mía, oscar.");
 		System.out.println("Hola soy Tania y voy a modificar esta clase");
 		System.out.println("Hola es mi segunda modificacion");
->>>>>>> branch 'master' of https://github.com/taniaruizsaenz/DAW.git
+
 	}
 
 }
