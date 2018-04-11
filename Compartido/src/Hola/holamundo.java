2c13159e29839caec278ce07c9533e73d885d1e8
Christian Ruiz <crnoain@gmail.com>
2018-04-11 17:52:57 +0200

    Nueva modificacion

--- Compartido/src/Hola/holamundo.java
+++ Compartido/src/Hola/holamundo.java
@@ -7,6 +7,7 @@ public class holamundo {
 		System.out.println("Hola, Buenas Tardes");
 		System.out.println("Hola, este soy yo, Oscar. Lo he modificado.");
 		System.out.println("Hola soy Tania, Buenas Tardes");
+		System.out.println("Hola soy Christian");
 	}
 
 }
