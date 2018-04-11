b581291730e36ea1da58d5008080343f42dc0433
Christian Ruiz <crnoain@gmail.com>
2018-04-11 17:06:13 +0200

    Añadido Metodo Nuevo

---
+++ Compartido/src/Hola/Saludar.java
@@ -0,0 +1,14 @@
+package Hola;
+
+public class Saludar {
+private String nombre;
+
+public void SaludarBien(String nombre){
+	this.nombre=nombre;
+	System.out.println();
+	
+}
+
+
+
+}
--- Compartido/src/Hola/holamundo.java
+++ Compartido/src/Hola/holamundo.java
@@ -2,8 +2,12 @@ package Hola;
 
 public class holamundo {
 	
+	
 	public static void main (String ars[]){
 		System.out.println("Hola, Buenas Tardes");
+		Saludar s= new Saludar();
+		s.SaludarBien("Tania");
+		
 	}
 
 }
