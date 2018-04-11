5b487f92573300f702f51292a5951005d75c4b8b
Vespertino <Vespertino@C8PC19>
2018-04-11 17:51:08 +0200

    modificacion tania3

--- Compartido/src/Hola/holamundo.java
+++ Compartido/src/Hola/holamundo.java
@@ -5,11 +5,8 @@ public class holamundo {
 	
 	public static void main (String ars[]){
 		System.out.println("Hola, Buenas Tardes");
-
-		
-
-System.out.println("Hola, este soy yo, Oscar. Lo he modificado.");
- 
+		System.out.println("Hola, este soy yo, Oscar. Lo he modificado.");
+		System.out.println("Hola soy Tania, Buenas Tardes");
 	}
 
 }
