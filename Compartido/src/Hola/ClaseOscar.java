d1cad5e1adc43cff994992b4f7be9b8d816cb910
Moretti <blog@standardinet.com>
2018-04-18 08:15:31 -0700

    A ver si sube la rama

--- Compartido/src/Hola/ClaseOscar.java
+++ Compartido/src/Hola/ClaseOscar.java
@@ -10,6 +10,11 @@ public class ClaseOscar {
 		System.out.println("Esta es otra modificación mía, oscar.");
 		System.out.println("Hola soy Tania y voy a modificar esta clase");
 		System.out.println("Hola es mi segunda modificacion");
+		System.out.println("A ver si va a unstaged...");
+		
+		System.out.println("Contar:");
+		for(int i = 0; i<=9 ; i++)
+			System.out.print(i);
 
 	}
 
