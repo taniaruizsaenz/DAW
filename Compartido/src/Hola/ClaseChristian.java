4c20aa58a3c37a7cad348e5c8901109e59cbab39
Christian Ruiz <crnoain@gmail.com>
2018-04-18 17:33:09 +0200

    Nuevo

--- Compartido/src/Hola/ClaseChristian.java
+++ Compartido/src/Hola/ClaseChristian.java
@@ -9,6 +9,14 @@ public static void main(String[] args) {
 		System.out.println("Hola soy Tania y voy a modificar esta clase");
 		System.out.println("Hola es mi segunda modificacion");
 		System.out.println("Hola soy Christian");
-		System.out.println("Hola es mi segunda modificacion");
+		System.out.println("Hola es mi segunda modificacion"); 
+		
+		String s[] = new String[5];
+		String aux= "hola";
+		for(int i=0;i<5;i++){
+			s[i]=aux;
+			System.out.println(s[i]);
+		}
+		
 	}
 }
